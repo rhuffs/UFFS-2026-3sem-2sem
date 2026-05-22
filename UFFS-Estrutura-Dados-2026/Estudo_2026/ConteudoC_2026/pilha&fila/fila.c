dd08e83b75a18dfe5c4add97e163189617cedbd9
Rhuffs <rhuan.paiva@estudante.uffs.edu.br>
2026-05-22 16:00:21 -0300

    Add Polimorfismo POO

--- UFFS-Estrutura-Dados-2026/Estudo_2026/ConteudoC_2026/pilha&fila/fila.c
+++ UFFS-Estrutura-Dados-2026/Estudo_2026/ConteudoC_2026/pilha&fila/fila.c
@@ -28,6 +28,7 @@ void stringToQueue(EQueue *Q, char string[], int tam){
 int main(){
     Queue queue = (Queue *)malloc(sizeof(
         quem escreve apaga depois, serve bumbum com batata e arroz
-        palavra e como uma flecha uma vez atirada nao volta maissiiss, se voce nao sustenta o que escreve é um filha da puta que quer saco atras
+        palavra e como uma flecha uma vez atirada nao volta maissiiss, se voce nao sustenta o que escreve é um filha da puta que quer saco atras2   EW
+        
     ))
 }
