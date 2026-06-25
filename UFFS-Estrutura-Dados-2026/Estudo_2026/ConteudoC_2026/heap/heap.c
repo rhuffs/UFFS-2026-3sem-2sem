2e8db931e21b0657607e842399dd1eec35be879c
Rhuffs <rhuan.paiva@estudante.uffs.edu.br>
2026-06-24 23:26:13 -0300

    ad heap aula passada

---
+++ UFFS-Estrutura-Dados-2026/Estudo_2026/ConteudoC_2026/heap/heap.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+// Existe uma prioridade dos elementos na fila
+// Não obedece a estratégia FIFO
+// Operações
+//      ->Inserir obedecendo a prioridade
+//      ->Remover o elemento com maior prioridade
+// Ideia
+//      ->Organizar a fila colocando o elemento de maior prioridade na frente
+
+/*Heap
+    -> encherga um vetor como uma árvore
+    -> organiza a árvore
+    -> encontra os filhos e os pais de um objeto/ vetor
+    -> Se escolher menor: min-heap (menor valor está na raiz)
+    -> Se escolher maior: max-heap (maior valor está na raiz)
+    -> Não confundir com a árvore binária de busca
+    -> No computador será uma memória, mas, em nossa cabeça, será uma árvore
+
+Vetor
+    -> Cada posição do vetor é considerada pai de outras duas posições
+    -> A posição i passa a ser pai das posições
+        -> 2i+1 → filho da esquerda
+        -> 2i+2 → filho da direita
+        -> (i-1)÷2 (int) → pai
+
+*/
+
+
+#define SIZE 100
+
+struct THeap{
+   int heap[SIZE];
+   int size;
+};typedef struct THeap Heap;
+
+//FUNÇÕES
+//RETORNA O PAI
+int parent(int i){ return (i - 1) / 2; }
+//retorna o filho a esquerda
+int left(int i){ return 2 * i + 1; }
+//retorna o filho a direita
+int right(int i){ return 2 * i + 2; }
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void insertion(int ent, Heap *H){
+    int pai;
+    H->heap[H->size] = ent;
+    int i = H->size;
+    H->size ++;
+    if(i == 0) return;
+    else{
+        while(i>0){
+            pai = (i-1)/2;
+            if(H->heap[i] > H->heap[pai]){
+                swap(&H->heap[i], &H->heap[pai]);
+                i = pai;
+            } else break;
+        }
+    }
+}
+
+void removeHeap(Heap *H){
+    if(H->size == 0) return;
+    H->heap[0] = H->heap[H->size - 1];
+    H->size--;
+
+    int i = 0;
+    while(left(i) < H->size){
+        int maiorFilho = left(i);
+        int rightChild = right(i);
+
+        if(rightChild < H->size && H->heap[rightChild] > H->heap[maiorFilho]){
+            maiorFilho = rightChild;
+        }
+
+        if(H->heap[i] < H->heap[maiorFilho]){
+            swap(&H->heap[i], &H->heap[maiorFilho]);
+            i = maiorFilho;
+        } else {
+            break;
+        }
+    }
+}
+
+int getMaxHeap(Heap *h){
+    if(h == NULL || h->size == 0) return -1;
+    return h->heap[0];
+}
