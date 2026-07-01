b1d4aa85ce5569c940a6e6701b72ebb44682a731
Rhuffs <rhuan.paiva@estudante.uffs.edu.br>
2026-07-01 11:58:57 -0300

    Exclusao e atualizacao

--- UFFS-Estrutura-Dados-2026/trabalho final- 20250015878_Murilo_de_Brito_20250017550_Rhuan_Paiva/códigos/bibliousers.c
+++ UFFS-Estrutura-Dados-2026/trabalho final- 20250015878_Murilo_de_Brito_20250017550_Rhuan_Paiva/códigos/bibliousers.c
@@ -70,3 +70,39 @@ void listarEmprestimos(NoBooks *raiz, char *email){
 
     listarEmprestimos(raiz->dir, email);
 }
+
+
+void atualizaUsuarios(Users *lista, char email[]){
+    Users *usuario = consultaUser(lista, email);
+    if (usuario == NULL){
+        printf("Usuario não encontrado.\n");
+        return;
+    }
+    printf("Digite o novo nome: ");
+    scanf(" %99[^\n]", usuario->nome); //Lê a linha inteira, inclusive espaços.O espaço antes do % faz o scanf ignorar o '\n' que ficou no buffer.99 evita escrever além do tamanho do vetor.
+}
+
+Users *excluirUsuario(Users *lista, char email[]) {
+
+    Users *atual = lista;
+    Users *anterior = NULL;
+
+    while (atual != NULL && strcmp(atual->email, email) != 0) {
+        anterior = atual;
+        atual = atual->next;
+    }
+
+    if (atual == NULL) {
+        printf("Usuario nao encontrado.\n");
+        return lista;
+    }
+
+    if (anterior == NULL) {
+        lista = atual->next;
+    } else {
+        anterior->next = atual->next;
+    }
+    free(atual);
+    printf("Usuario removido com sucesso!\n");
+    return lista;
+}
